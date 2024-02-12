208b8db864359d627b040e0f2499c5ec3e136203
Dele <ajayibamidele60@gmail.com>
2024-02-12 14:48:33 +0100

    email validation and modal output fixed

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
 let emailField = document.getElementById("email-field");
 let errorEmail = document.getElementById("error-email");
+let btn1 = document.getElementById("btn1")
+let emailInput = document.getElementById("email-input")
+let popup = document.getElementById("popup")
 
-function validateEmail() {
+function submit() {
     if(!emailField.value.match(/^[A-Za-z\._\-0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)) {
         errorEmail.textContent = "Valid email required";
         emailField.style.borderColor = "red";
         emailField.style.backgroundColor = "#FFCCCB"
         return false;
     } else
+    popup.classList.add("open-popup");
     errorEmail.textContent = "";
-    errorEmail.style.color = "green"
-    emailField.style.borderColor = "green"
-    emailField.style.backgroundColor = "#90EE90"
     return true;
 }
+
+function closePopup() {
+    popup.classList.remove("open-popup");
+}
+
+function input() {
+    emailInput.textContent = emailField.value;
+    emailInput.style.fontWeight = "bold";
+    emailInput.style.color = "#242742";
+}
+
+btn1.addEventListener('click',input);
