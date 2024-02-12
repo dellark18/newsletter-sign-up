7afd765d0bd575c03c2ebb12fdbd32f67be9375a
Dele <ajayibamidele60@gmail.com>
2024-02-12 10:16:14 +0100

    email validation fixed

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-function submit() {
-    const email = getElementById("email").value;
-    var regEx = /\S+@\S+\.\S+/; i 
-    if (regEx.test(email)) {
-        document.getElementById("errorNotification").textContent = "Correct email";
-    } else {
-        document.getElementById("errorNotification").textContent = "Valid email required"
-        errorNotification.toggleAttribute("hidden");
-    }
+let emailField = document.getElementById("email-field");
+let errorEmail = document.getElementById("error-email");
+
+function validateEmail() {
+    if(!emailField.value.match(/^[A-Za-z\._\-0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)) {
+        errorEmail.textContent = "Valid email required";
+        emailField.style.borderColor = "red";
+        emailField.style.backgroundColor = "#FFCCCB"
+        return false;
+    } else
+    errorEmail.textContent = "";
+    errorEmail.style.color = "green"
+    emailField.style.borderColor = "green"
+    emailField.style.backgroundColor = "#90EE90"
+    return true;
 }
